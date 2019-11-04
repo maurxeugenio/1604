4ef30cedd8150b4617adf28345e56a97f4f941ee
Maurício Eugênio <maurxeugenio@gmail.com>
2019-11-04 16:04:16 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <h1 style={{ fontWeight: 500, padding: 0, margin: 0, fontSize: '50pt'}}>Bem-vindo,</h1>
         <h2 style={{ fontWeight: 300}}>
           Estamos em fase inicial, portanto ainda não temos nada por aqui.
-          Porém você pode entrar em contato, através dos meios de comunicação abaixo.
+          Porém você pode entrar em contato, através dos meios de comunicação listados abaixo.
         </h2>
         <div style={{display: 'flex', flexDirection: 'row-responsive' }}>
           <Link target="_blank" style={{padding: '0 10px 0 0 '}} href="https://api.whatsapp.com/send?phone=5533998315066&text=Ol%C3%A1%2C%20fiquei%20interessado%20sobre%20a%201604%2C%20afinal%20o%20que%20voc%C3%AAs%20fazem%3F">
