f61127f55f4695c7fe4c67a61b714a6e744bb056
maurxeugenio <maurxeugenio@gmail.com>
2020-07-28 11:04:17 -0300

    Updates

---
+++ precache-manifest.e801f986b44c4d75fc185ae13050f6d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d6734392b25420e7a3c9ab7096b7b50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed3f08215cab786c9da6",
+    "url": "/static/css/main.d8376d57.chunk.css"
+  },
+  {
+    "revision": "5f0628f657e6188049da",
+    "url": "/static/js/2.9bc556af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9bc556af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed3f08215cab786c9da6",
+    "url": "/static/js/main.9c95aef7.chunk.js"
+  },
+  {
+    "revision": "7f52e56aab537f94b420",
+    "url": "/static/js/runtime-main.dfd96d71.js"
+  }
+]);
